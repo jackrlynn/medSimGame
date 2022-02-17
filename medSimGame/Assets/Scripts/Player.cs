ec9020ae3728a6f0bca099c9e8559176a39921a0
Cole Garton <cole-garton@uiowa.edu>
2022-02-16 23:33:47 -0600

    Vertical Camera Movement Fix
    2022.2.16 - Cole Garton
    
    test push for vertical camera movement

--- medSimGame/Assets/Scripts/Player.cs
+++ medSimGame/Assets/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ff67841acff18516def6c843ab256792f4c9f63319e5e8911ac245074a6fe2b
-size 4412
+oid sha256:aeec97c38a677e550ca2f76dcb005840697b47c81e68e6c4d75b3d1c9dfc978b
+size 4553
