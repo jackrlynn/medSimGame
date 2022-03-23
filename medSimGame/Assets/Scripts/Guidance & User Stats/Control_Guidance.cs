f43d3624f5783b21d5992a82eb4f491aae764271
Cyril Zhang <zhangsiyu1107@gmail.com>
2022-03-23 10:38:28 -0500

    update restart guidance button

--- medSimGame/Assets/Scripts/Guidance & User Stats/Control_Guidance.cs
+++ medSimGame/Assets/Scripts/Guidance & User Stats/Control_Guidance.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:969f281fc3c9a2d7681425078defc0d6366ab7211f9f1392d3750a88f86a1364
-size 1899
+oid sha256:c4456da8a0e181814455ee6f753568f8156efe144647cfbbbbc7bb815e7b3d23
+size 2000
--- medSimGame/Assets/Scripts/Guidance & User Stats/Status.cs
+++ medSimGame/Assets/Scripts/Guidance & User Stats/Status.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d92b6c60c885ef2ee1985d057b340894f87199cb81a86a5e37bfaa2cffa49a74
-size 1085
+oid sha256:66155a1430f9bba83618f4e55685d3fe4661d826145ff23bccbad48836f89d9a
+size 1574
