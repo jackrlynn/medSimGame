e25ec28b5cadf52aebb61510578cfab6c49f38a3
jackrlynn <jacklynn@uiowa.edu>
2022-03-23 21:59:20 -0500

    Rotation Movement
    2022.3.23 - Jack Lynn
    Added basic object rotation functionality using GameObject.transform.Rotate(x-axis, y-axis, z-axis, Space.Self); functionality added for rotation around x-, y-, and z-axes.

--- medSimGame/Assets/Scripts/HeartPlayer.cs
+++ medSimGame/Assets/Scripts/HeartPlayer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8908c870c135d037262ac270f6bbdaed04c991315a8c9733204c568f694bd81
-size 937
+oid sha256:e7a96784dc621a7844fd5ccaf085b2e54c31a246322e7063d7b9517ea37a5274
+size 2689
