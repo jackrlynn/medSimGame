b9a662e92c15534fc4eaa57e7399752ebee12a60
jackrlynn <jacklynn@uiowa.edu>
2022-03-24 00:30:51 -0500

    Rotation Now Based Around Camera
    2022.3.24 - Jack Lynn
    I tested several methods for rotating around the heart:
    - The heart rotates itself >>> Get's really awkward after awhile
    - The camera rotates circularly around the heart >>> Same awkwardness as before
    - The camera moves freely and can rotate up and down >>> best option
    I began to implement the last option, but it will take some finessing to see what works best for the most intuitive use.

--- medSimGame/Assets/Scripts/HeartPlayer.cs
+++ medSimGame/Assets/Scripts/HeartPlayer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7a96784dc621a7844fd5ccaf085b2e54c31a246322e7063d7b9517ea37a5274
-size 2689
+oid sha256:ee912ae22d9f0766d98d4e1372acdee934d0e2a11b1524a8aa838f9512144598
+size 3576
